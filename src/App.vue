547130b571143aa6299aba2b76e40ca7f082addf
Fernando Junior <fernando.junior85@outlook.com>
2021-11-10 17:05:37 -0300

    Card created and consuming API

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,71 @@
 <template>
   <div id="app">
-    <h1>Hello World</h1>
+    <div class="search">
+      <input
+        type="text"
+        placeholder="Digite um CEP..."
+        v-model="cep"
+        maxlength="8"
+      />
+      <button @click="getCep">Pesquisar</button>
+    </div>
+    <Card :dados="dados" />
   </div>
 </template>
 
 <script>
+import Card from './components/Card'
+
 export default {
-  name: "App",
-  components: {},
-  created: {},
-};
+  name: 'App',
+  components: {
+    Card,
+  },
+  data: () => ({
+    cep: '',
+    dados: {},
+  }),
+  methods: {
+    getCep() {
+      fetch(`https://viacep.com.br/ws/${this.cep}/json/`)
+        .then((res) => res.json())
+        .then((data) => (this.dados = data))
+      this.cep = ''
+    },
+  },
+}
 </script>
 
-<style></style>
+<style scoped>
+#app {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 60px;
+  background-color: lightblue;
+}
+
+.search input {
+  border: none;
+  height: 40px;
+  outline: none;
+  padding: 5px 10px;
+  font-size: 16px;
+}
+
+.search button {
+  background-color: lightcoral;
+  height: 40px;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  font-size: 16px;
+  font-weight: 600;
+  color: #fff;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="card">
+    <input type="text" placeholder="CEP" disabled :value="dados.cep" />
+    <input type="text" placeholder="UF" disabled :value="dados.uf" />
+    <input
+      type="text"
+      placeholder="Cidade"
+      disabled
+      :value="dados.localidade"
+    />
+    <input
+      type="text"
+      placeholder="Logradouro"
+      disabled
+      :value="dados.logradouro"
+    />
+    <input type="text" placeholder="Bairro" disabled :value="dados.bairro" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    dados: {
+      type: Object,
+      required: false,
+    },
+  },
+}
+</script>
+
+<style>
+.card {
+  max-width: 500px;
+  width: 90%;
+  margin: 0 auto;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  padding: 20px;
+  border-left: 10px solid lightcoral;
+}
+
+.card input {
+  width: 100%;
+  height: 40px;
+  padding: 10px;
+}
+
+.card__logradouro {
+  display: flex;
+  align-items: center;
+  gap: 5px;
+}
+
+@media screen and (min-width: 768px) {
+  .card input {
+    width: 60%;
+    margin: 0 auto;
+  }
+}
+</style>
